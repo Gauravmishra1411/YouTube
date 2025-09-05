0af5b4907225063091160facf61a1a4ea15c7ed3
Gauravmishra <iamgauravdbg@gmail.com>
2025-09-05 10:38:33 +0530

    goaurav

--- src/Componet/WatchVedio.js
+++ src/Componet/WatchVedio.js
@@ -1,32 +1,66 @@
- import { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../util/Slice/appSlice'
 import { useParams } from "react-router-dom";
-
+import { Link } from 'react-router-dom';
+import { useCustom } from '../util/costom/UsedCostam';
 const WatchVedio = () => {
   const dispatch = useDispatch();
   const { id } = useParams(); // ✅ correctly destructure
-
+  const vedios = useCustom()
+  console.log("--1-2-3---->",vedios);
+  
   useEffect(() => {
     console.log("-> id:", id);
     dispatch(closeMenu());
   }, [dispatch, id]);
 
   return (
-    <div>
-      {id ? (
-        <iframe
-          width="1050"
-          height="500"
-          src={`https://www.youtube.com/embed/${id}`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
+
+    <div className='flex justify-start align-baseline  '>
+      <div className='mx-20 pt-5' >
+        {id ? (
+          <iframe
+            width="1050"
+            className='sticky top-0'
+            height="500"
+            src={`https://www.youtube.com/embed/${id}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        ) : (
+          <p>No video selected</p>
+        )}
+      </div>
+      <div className=' mr-32 '>
+        {vedios.map((item, index) => {
+  const {
+    id, // sometimes YouTube API nests id as { videoId }
+    snippet: {
+      title,
+      thumbnails: { standard },
+    },
+  } = item;
+
+  return (
+    <Link key={index} to={`/watch/`+id}>
+      <div className="aspect-video">
+        <img
+          alt={title}
+          src={standard?.url}
+          className="w-80 h-30 object-cover   rounded-lg mx-7 pt-4 shadow-xl"
         />
-      ) : (
-        <p>No video selected</p>
-      )}
+        <p className="text-sm font-semibold text-nowrap">{title.slice(1,50)}</p>
+      </div>
+    </Link>
+  );
+ 
+
+           
+        })}
+      </div>
     </div>
   );
 };
